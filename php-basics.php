ee3b4fdfa47552126852fa1dea11979e6438383e
unknown <your@gmail.exe>
2024-09-13 10:55:50 +0300

    ph CLI scripts

---
+++ php-basics.php
@@ -0,0 +1,11 @@
+<?php
+echo "This is my first php script";
+
+
+
+// 1.uzd
+//izveido cliku, lai izvaditu skaitlus no 1-10. Izvadit skaitlus no 10 lidz 1.
+
+// for ($x = 0; $x <= 10; $x++) {
+//     echo "The number is: $x <br>";
+//   }
---
+++ to-do-list.php
@@ -0,0 +1,48 @@
+<?php
+
+$taskList = ["Fisrt task", "Second task"];
+
+
+function showTask($inputTasks) {
+    foreach($inputTasks as $task);
+
+}
+
+// $name = readline("ievadi savu vardu:\n");
+// echo "your name is $name";
+
+
+// 1.Uzdevums 
+// Uztasit do while cilku, ko lietotajs
+// var apstadinat ievadot 'N'
+
+// 2. Uzdevums 
+// izveidot 
+
+do {
+    echo "uzdevumu parvaldnieks 1\n";
+    echo "Apskatit uzdevumu => 2\n";
+    echo "Ievadit jaunu uzdevumu => 3\n";
+
+
+    $choice = readline("izvelies darbibiu:");
+
+  } while ($choice != "N");
+
+  switch ($choice) {
+  case '1':
+    echo 'To be implemented ';
+    break;
+  case '2':
+    echo 'Your favorite color is blue!';
+    break;
+  case '3':
+    echo 'Your favorite color is green!';
+    break;
+  default:
+    echo 'Invalid option! \n';
+
+    function addTask($inputTasks){
+        echo "to be implemented"
+    }
+}
